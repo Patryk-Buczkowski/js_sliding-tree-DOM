f022bf60ceb2004a0c33457231346d33b51513ef
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-26 13:28:01 +0100

    make build

---
+++ index.efe4eca8.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");e.querySelectorAll("li").forEach(function(e){var t=document.createElement("span");t.className="list-item",e.prepend(t),t.append(t.nextSibling)}),e.onclick=function(e){if("SPAN"===e.target.tagName){var t=e.target.parentElement.querySelector("ul");t&&t.classList.toggle("visible")}};//# sourceMappingURL=index.efe4eca8.js.map
+
+//# sourceMappingURL=index.efe4eca8.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const tree = document.querySelector(".tree");
+const li = tree.querySelectorAll("li");
+li.forEach((elem)=>{
+    const span = document.createElement("span");
+    span.className = "list-item";
+    elem.prepend(span);
+    span.append(span.nextSibling);
+});
+tree.onclick = function(e) {
+    if (e.target.tagName === "SPAN") {
+        const ulElem = e.target.parentElement.querySelector("ul");
+        if (ulElem) ulElem.classList.toggle("visible");
+    }
+};
+
+//# sourceMappingURL=index.f75de5e1.js.map
